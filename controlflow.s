70edf01c087827d98ddf6a7ed4b19aa7a2018f52
mgregoire254 <mgregoire1@me.com>
2023-10-23 13:40:57 -0500

    modify control flow practice

--- controlflow.s
+++ controlflow.s
@@ -1,7 +1,8 @@
 ; Practicing control flow in 6502 assembly.
 
 ; Write an assembly snippet that loads the Accumulator with the value 10, 
-; then decrements it until it reaches zero or becomes negative. If it becomes negative, branch to a label called negative_value.
+; then decrement it until it reaches zero or becomes negative. If it becomes negative, branch to a label called negative_value.
 
+LDA #10 ; Load accumulator with the value of 10
 
 ; Create a subroutine named multiply_by_two. It should load a value into the Accumulator, double it, and then return.
